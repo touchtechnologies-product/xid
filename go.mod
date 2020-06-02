155099cc7184b72d4723dcf31e251c52be3db850
Chat Won By m <chat.w@touchtechnologies.co.th>
2020-06-02 18:16:42 +0700

    add

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/touchtechnologies-product/xid
+
+go 1.14
+
+require (
+	github.com/rs/xid v1.2.1
+	github.com/stretchr/testify v1.6.0
+)
---
+++ goxid.go
@@ -0,0 +1,31 @@
+package goxid
+
+import (
+	"github.com/rs/xid"
+)
+
+type ID struct {
+	freezeID string
+}
+
+var instance *ID
+
+func New() *ID {
+	if instance == nil {
+		instance = &ID{}
+	}
+
+	return instance
+}
+
+func (i *ID) Gen() string {
+	if i.freezeID != "" {
+		return i.freezeID
+	}
+
+	return xid.New().String()
+}
+
+func (i *ID) Freeze(id string) {
+	i.freezeID = id
+}
